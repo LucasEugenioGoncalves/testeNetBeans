93775d076dd9658e4a06230017e77b15e3febd6e
lucas17943 <lucas17943@gmail.com>
2024-02-06 08:06:47 -0300

    teste 12345

--- pix.php
+++ pix.php
@@ -59,7 +59,7 @@ if(isset($_SESSION['username'])) {
 </head>
 <body>
     <div class="container">
-        <h2>Login</h2>
+        <h2>Funcionou... faz o PIX</h2>
         <form action="login.php" method="post">
             <input type="text" name="username" placeholder="Username" required>
             <input type="password" name="password" placeholder="Password" required>
